ed93fcc1bf61cb8c1050670dfb1af9992d7385b9
Shaobi Li <shaobili@gmail.com>
2014-05-30 02:34:12 -0700

    add the solution to part2, add the driver, edit the README file

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ scopely-challenge
 =================
 
 scopely coding challenge
+
+
+part 1:
+ruby answer.rb -a1 -p [PATH]
+
+
+part 2:
+ruby answer.rb -a2 -p [PATH]
+
+note: this will use the PATH to generate the tree and then convert the tree to a path.
+
+
+part 3:
+ruby answer.rb -a3 -p [PATH] -i [INPUT]
+
+note: this will use the PATH to generate the tree and then print out the synonym. 
+
---
+++ answer.rb
@@ -0,0 +1,165 @@
+#!/usr/bin/env ruby
+
+# part1 solver
+module Solver1
+  class << self
+    def make_tree(path)
+      arr = path.partition('/')
+      raise 'invalid format' unless arr[0].empty?
+      tree = make_node('', arr[2])
+      return tree.children[0]
+    end
+
+    def pp_dfs(tree)
+      arr = []
+      pp_dfs_helper(tree, arr)
+      return arr.inspect
+    end
+
+    private
+    def pp_dfs_helper(tree, arr)
+      arr << tree.data
+      tree.children.each do |child|
+        new_arr = []
+        arr << new_arr
+        pp_dfs_helper(child, new_arr)
+      end
+    end
+
+    def combination(arr, n)
+      ret = []
+      0.upto(arr.size - n) do |i|
+        temp = []
+        i.upto(i+n-1) do |j|  
+          temp << arr[j]
+        end
+        ret << temp
+      end
+      return ret
+    end
+
+    def gen_comb(arr)
+      ret = []
+      1.upto(arr.size) do |i|
+        combination(arr, i).each do |e|
+          ret << e
+        end
+      end
+      return ret
+    end
+
+    def format_comb(arr)
+      ret = []
+      arr.each do |sub_arr|
+        if sub_arr.size > 1
+          # use dash to connect the multiple strings         
+          ret << sub_arr.inject('') {|acc, e| acc + e + '-' }[0..-2]
+        elsif sub_arr.size == 1
+          ret << sub_arr[0]
+        else
+          # do nothing...
+        end
+      end
+      return ret
+    end
+
+    def parse_a_slash(str)
+      arr = str.partition('/')
+      return [arr[0], arr[2]]
+    end
+
+    def parse_comb(str)
+      return str.split('|')
+    end
+
+    def make_node(data, path)
+      tree = CTree.new(data)
+      nxt, rem = parse_a_slash(path)
+      nxt = parse_comb(nxt)
+      nxt = gen_comb(nxt.reverse)
+      nxt = format_comb(nxt)
+      nxt.each do |data|
+        tree.add_child(make_node(data, rem))
+      end
+      return tree
+    end
+  end
+end
+
+class CTree
+  attr_reader :data
+  def initialize(data)
+    @data = data
+    @nodes = []
+  end
+
+  def add_child(node)
+    @nodes << node
+  end
+
+  def children
+    @nodes
+  end
+
+  # this is the general DFS that can pass in a block
+  def dfs(&block)
+    yield @data
+    @nodes.each do |node|
+      node.dfs(&block)
+    end
+  end
+end
+
+# part2 solver
+module Solver2
+  class << self
+    def tree2path(tree)
+      dummytree = CTree.new('')
+      dummytree.add_child(tree)
+      path = ''
+      tree2path_helper(dummytree, '', path)
+      return path[1..-1]
+    end
+
+    private
+    # this part we need to 'lift' the recursion one level up
+    # so we can collect all the siblings. because of the natural
+    # feature of the combination tree, we can always choose any
+    # one child to perform DFS safely.
+    def tree2path_helper(tree, current, path)
+      path << '/' << current
+      if !tree.children.empty?
+        set = []
+        tree.children.each do |child|
+          set << child.data unless child.data.include?('-')
+        end
+        acc = set.inject('') {|acc, e| acc + e + '|'}[0..-2]
+        tree2path_helper(tree.children[0], acc, path)
+      end
+    end
+  end
+end
+
+def usage
+  puts 'usage:'
+  puts 'answer.rb [-a1|-a2|-a3] [-p path]'
+end
+
+# part3 solver
+if ARGV[0] == '-a1' || ARGV[0] == '-a2' || ARGV[0] == '-a3'
+  if ARGV[1] == '-p'
+    path = ARGV[2]
+  else
+    usage
+  end
+  tree = Solver1.make_tree(path)
+  if ARGV[0] == '-a1'
+    puts Solver1.pp_dfs(tree)
+  elsif ARGV[0] == '-a2'
+    puts Solver2.tree2path(tree)
+  else
+    puts 'not implement'
+  end
+else
+  usage
+end
